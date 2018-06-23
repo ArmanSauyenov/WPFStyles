0469947a4c31797e4844136e96e3097f652f30e6
Arman KZ <armansauyenov@gmail.com>
2018-06-23 15:23:34 +0600

    Add project files.

---
+++ styles/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace styles
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+      
+        private void Change_Template(object sender, RoutedEventArgs e)
+        {
+            ResourceDictionary template = new ResourceDictionary();
+
+            RadioButton rb = (RadioButton)sender;
+
+            switch (rb.Name)
+            {
+                case "Dark":
+                    {
+                        template.Source = new Uri("/Resourses/DarkStyle.xaml", UriKind.Relative);
+                    }
+                    break;
+                case "Light":
+                    {
+                        template.Source = new Uri("/Resourses/Style.xaml", UriKind.Relative);
+                    }
+                    break;
+            }
+
+         //   var currentResourse = this.Resources.MergedDictionaries;
+            this.Resources.MergedDictionaries[0] = template;
+
+        }
+    }
+}
